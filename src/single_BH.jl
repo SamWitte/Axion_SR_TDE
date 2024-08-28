2cbffd5a4eaf1d98030115600a9a3a6a76ed7040
Samuel Witte <witte.sam@gmail.com>
2024-08-28 09:48:34 +0200

    no time var

--- src/single_BH.jl
+++ src/single_BH.jl
@@ -53,33 +53,33 @@ fname *= "fa_$(f_a)_ma_$(m_a)_MBH_$(MassBH)_spin_$(SpinBH)"*fout*".dat"
 
 
 if !solve_n4
-    time, state211, state322, spin, massB = @time solve_system(m_a, f_a, SpinBH, MassBH, tau_max; impose_low_cut=impose_low_cut, return_all_info=return_all_info, solve_322=solve_322, n_times=n_times, input_data=input_data, eq_threshold=eq_threshold, stop_on_a=stop_on_a, debug=true, abstol=abstol, non_rel=non_rel, max_m_2=max_m_2)
+    timeT, state211, state322, spin, massB = @time solve_system(m_a, f_a, SpinBH, MassBH, tau_max; impose_low_cut=impose_low_cut, return_all_info=return_all_info, solve_322=solve_322, n_times=n_times, input_data=input_data, eq_threshold=eq_threshold, stop_on_a=stop_on_a, debug=true, abstol=abstol, non_rel=non_rel, max_m_2=max_m_2)
 else
     if !solve_n5
-        time, state211, state322, state411, state422, state433, spin, massB = @time solve_system(m_a, f_a, SpinBH, MassBH, tau_max; impose_low_cut=impose_low_cut, return_all_info=return_all_info, solve_322=solve_322, n_times=n_times, input_data=input_data, solve_n4=true, eq_threshold=eq_threshold, abstol=abstol, non_rel=non_rel, debug=true, max_m_2=max_m_2)
+        timeT, state211, state322, state411, state422, state433, spin, massB = @time solve_system(m_a, f_a, SpinBH, MassBH, tau_max; impose_low_cut=impose_low_cut, return_all_info=return_all_info, solve_322=solve_322, n_times=n_times, input_data=input_data, solve_n4=true, eq_threshold=eq_threshold, abstol=abstol, non_rel=non_rel, debug=true, max_m_2=max_m_2)
     else
-        time, state211, state322, state411, state422, state433, state522, state533, state544, spin, massB = @time solve_system(m_a, f_a, SpinBH, MassBH, tau_max; impose_low_cut=impose_low_cut, return_all_info=return_all_info, solve_322=solve_322, n_times=n_times, input_data=input_data, solve_n4=true, solve_n5=true, eq_threshold=eq_threshold, abstol=abstol, non_rel=non_rel, debug=true, max_m_2=max_m_2)
+        timeT, state211, state322, state411, state422, state433, state522, state533, state544, spin, massB = @time solve_system(m_a, f_a, SpinBH, MassBH, tau_max; impose_low_cut=impose_low_cut, return_all_info=return_all_info, solve_322=solve_322, n_times=n_times, input_data=input_data, solve_n4=true, solve_n5=true, eq_threshold=eq_threshold, abstol=abstol, non_rel=non_rel, debug=true, max_m_2=max_m_2)
     end
 end
 
 if sve
     if !solve_n4
-        outSve = zeros(length(time), 5)
-        for i in 1:length(time)
-            outSve[i, :] = [time[i] state211[i] state322[i] spin[i] massB[i]]
+        outSve = zeros(length(timeT), 5)
+        for i in 1:length(timeT)
+            outSve[i, :] = [timeT[i] state211[i] state322[i] spin[i] massB[i]]
         end
         writedlm(fname, outSve)
     else
         if !solve_n5
-            outSve = zeros(length(time), 8)
-            for i in 1:length(time)
-                outSve[i, :] = [time[i] state211[i] state322[i] state411[i] state422[i] state433[i] spin[i] massB[i]]
+            outSve = zeros(length(timeT), 8)
+            for i in 1:length(timeT)
+                outSve[i, :] = [timeT[i] state211[i] state322[i] state411[i] state422[i] state433[i] spin[i] massB[i]]
             end
             writedlm(fname, outSve)
         else
-            outSve = zeros(length(time), 11)
-            for i in 1:length(time)
-                outSve[i, :] = [time[i] state211[i] state322[i] state411[i] state422[i] state433[i] state522[i] state533[i] state544[i] spin[i] massB[i]]
+            outSve = zeros(length(timeT), 11)
+            for i in 1:length(timeT)
+                outSve[i, :] = [timeT[i] state211[i] state322[i] state411[i] state422[i] state433[i] state522[i] state533[i] state544[i] spin[i] massB[i]]
             end
             writedlm(fname, outSve)
         end
