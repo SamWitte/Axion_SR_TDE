edb1afe8e78f7162c58f0108158ba9d5e5cb7202
Samuel Witte <witte.sam@gmail.com>
2024-10-07 21:21:57 +0100

    trying something

--- src/test_radial_nl_diff.jl
+++ src/test_radial_nl_diff.jl
@@ -23,12 +23,13 @@ rmaxT=500
 Ntot_safe=5000
 sve_for_test=false
 add_source=true
-eps_fac= BigFloat(1e-4)
+eps_fac= BigFloat(1e-2)
 dtmax_slv = 1e-1
 reltol=1e-10
 Nang=300000
 n_times=300000
 NON_REL = true
+h_mve=0.1
 
 m=0
 l=0
@@ -46,6 +47,6 @@ print("NON_REL \t", NON_REL, "\n")
 print("l \t", l, "\n")
 
 
-@time integrate_radialEq_2(mu, M, a, n1, l1, m1, n2, l2, m2, n3, l3, m3; rpts=rpts, Npts_Bnd=Npts_Bnd, rmaxT=rmaxT, debug=true, sve_for_test=sve_for_test, add_source=add_source, eps_fac=eps_fac, Ntot_safe=Ntot_safe, dtmax_slv=dtmax_slv, m=m, l=l, reltol=reltol, Nang=Nang, NON_REL=NON_REL, n_times=n_times)
+@time integrate_radialEq_2(mu, M, a, n1, l1, m1, n2, l2, m2, n3, l3, m3; rpts=rpts, Npts_Bnd=Npts_Bnd, rmaxT=rmaxT, debug=true, sve_for_test=sve_for_test, add_source=add_source, eps_fac=eps_fac, Ntot_safe=Ntot_safe, dtmax_slv=dtmax_slv, m=m, l=l, reltol=reltol, Nang=Nang, NON_REL=NON_REL, n_times=n_times, h_mve=h_mve)
 
 
