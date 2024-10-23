8b53cc7f71aeea91594fb343ba993aa8fcd22e91
Samuel Witte <witte.sam@gmail.com>
2024-10-23 15:16:22 +0200

    update rel computer

--- src/test_radial_nl_diff.jl
+++ src/test_radial_nl_diff.jl
@@ -1,35 +1,33 @@
 include("solve_sr_rates.jl")
 
-mu = 2.5e-13
+aa = 0.1
+# mu = 3e-13
 
 M=22.2
 a=0.9
+mu = aa ./ (GNew .* M)
 
-n1=2
-l1=1
-m1=1
+n1=3
+l1=2
+m1=2
 
-n2=2
-l2=1
-m2=1
+n2=3
+l2=2
+m2=2
 
-n3=3
-l3=2
-m3=2
+n3=2
+l3=1
+m3=1
 
-rpts=50000
-Npts_Bnd=4000
-rmaxT=500
+to_inf = true
+
+rpts=500000
+Npts_Bnd=50000
 Ntot_safe=5000
-sve_for_test=false
-add_source=true
-eps_fac= BigFloat(1e-2)
-dtmax_slv = 1e-1
-reltol=1e-10
+eps_fac= BigFloat(1e-3)
 Nang=300000
-n_times=300000
-NON_REL = true
-h_mve=0.1
+NON_REL = true ## quick approx radial WF and energies
+h_mve=10
 
 m=0
 l=0
@@ -38,15 +36,15 @@ print("Params for Solving non-lin KG: \n")
 print("Alpha \t", GNew .* M .* mu, "\n")
 print("spin \t", a, "\n")
 print("rpts \t", rpts, "\n")
-print("rmaxT \t", rmaxT, "\n")
-print("add_source \t", add_source, "\n")
 print("eps_fac \t", eps_fac, "\n")
-print("dtmax_slv \t", dtmax_slv, "\n")
-print("reltol \t", reltol, "\n")
 print("NON_REL \t", NON_REL, "\n")
 print("l \t", l, "\n")
+print("H mve \t", h_mve, "\n")
+print("Rate \t", n1, l1, m1, "\t", n2,l2,m2, "\t", n3,l3,m3, "\n")
 
+# @time integrate_radialEq_2(mu, M, a, n1, l1, m1, n2, l2, m2, n3, l3, m3; rpts=rpts, Npts_Bnd=Npts_Bnd, debug=true,  eps_fac=eps_fac, Ntot_safe=Ntot_safe, m=m, l=l, Nang=Nang, NON_REL=NON_REL,  h_mve=h_mve)
 
-@time integrate_radialEq_2(mu, M, a, n1, l1, m1, n2, l2, m2, n3, l3, m3; rpts=rpts, Npts_Bnd=Npts_Bnd, rmaxT=rmaxT, debug=true, sve_for_test=sve_for_test, add_source=add_source, eps_fac=eps_fac, Ntot_safe=Ntot_safe, dtmax_slv=dtmax_slv, m=m, l=l, reltol=reltol, Nang=Nang, NON_REL=NON_REL, n_times=n_times, h_mve=h_mve)
+# @time integrate_radialEq_3(mu, M, a, n1, l1, m1, n2, l2, m2, n3, l3, m3; rpts=rpts, Npts_Bnd=Npts_Bnd, , debug=true, sve_for_test=sve_for_test, add_source=add_source, eps_fac=eps_fac, Ntot_safe=Ntot_safe, m=m, l=l, Nang=Nang, NON_REL=NON_REL,  h_mve=h_mve)
 
+@time gf_radial(mu, M, a, n1, l1, m1, n2, l2, m2, n3, l3, m3; rpts=rpts, Npts_Bnd=Npts_Bnd, debug=true,  eps_fac=eps_fac, Ntot_safe=Ntot_safe, m=m, l=l, Nang=Nang, NON_REL=NON_REL,  h_mve=h_mve, to_inf=to_inf)
 
